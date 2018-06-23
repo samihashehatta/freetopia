8c5d29374d815718dd33e72219d06755aa661478
samiha <samihashehatta@gmail.com>
2018-06-23 16:51:40 +0200

    adding freetopia project

---
+++ top freelancers/js/plugins.js
@@ -0,0 +1,15 @@
+/*Select Box js*/
+$('.drop-menu').click(function () {
+        $(this).attr('tabindex', 1).focus();
+        $(this).toggleClass('active');
+        $(this).find('.dropeddown').slideToggle(300);
+    });
+    $('.drop-menu').focusout(function () {
+        $(this).removeClass('active');
+        $(this).find('.dropeddown').slideUp(300);
+    });
+    $('.drop-menu .dropeddown li').click(function () {
+        $(this).parents('.drop-menu').find('span').text($(this).text());
+        $(this).parents('.drop-menu').find('input').attr('value', $(this).attr('id'));
+    });
+/*End Select Box js*/
